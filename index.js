fbedf607066250775e2f9c75a33ccfd570299b54
Özlem Keleş <ozlemkeles98@gmail.com>
2023-03-20 13:16:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    const messages = [];
+
+    for(let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number--);
+    }
+}
+
+countDown(10);
